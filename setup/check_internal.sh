f1d40da5b2d12b47fdc886cb2835718bea74abcb
Felix Hassert <felix.hassert@sevenval.com>
2014-09-23 19:21:03 +0200

    update to final build 14.0.1 box

--- setup/check_internal.sh
+++ setup/check_internal.sh
@@ -209,7 +209,7 @@ fi
 
 # ====================================================================================================
 
-EXP_VERSION="14-0-1-c2"
+EXP_VERSION="14-0-1"
 FIT_VERSION=`fitadmin -v | head -n1`
 
 sMessage="FIT Version '$EXP_VERSION' (is: $FIT_VERSION)"
