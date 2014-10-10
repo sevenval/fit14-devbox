4bac35d9595f071346e79539fe58fcbef30172e6
Felix Hassert <felix.hassert@sevenval.com>
2014-10-10 13:35:55 +0200

    `./setup/which-version.sh` gibt die aktuelle FIT version und den abonnierten channel aus

---
+++ setup/which-version.sh
@@ -0,0 +1,8 @@
+
+CMD="sudo /opt/sevenval/fit14/bin/fitadmin -v; echo -n \"Channel: \"; yum --noplugins --quiet repolist enabled |grep fit14 |cut -d/ -f1 "
+if [ -f /vagrant/setup/switch-version-internal.sh ]; then
+	sh -c "$CMD"
+else
+	vagrant ssh -c "$CMD" -- -q
+fi
+
