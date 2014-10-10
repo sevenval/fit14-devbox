235b4ec111b63a27b2cd115cd0cd71efb1e9bf4a
Felix Hassert <felix.hassert@sevenval.com>
2014-10-10 13:33:50 +0200

    * uebergebene parameter checken, wenn z.b. `switch-version.sh fit14daily` aufgerufen wird * fehlermeldungen nach stderr * vagrant ssh -q, damit keine connection meldungen ausgegeben werde und die ausgabe verhageln

--- setup/switch-version.sh
+++ setup/switch-version.sh
@@ -8,13 +8,20 @@ else
 	"n") VERSION=fit14nightly ;;
 	"d") VERSION=fit14devel ;;
 	"s") VERSION=fit14stable ;;
-	*) echo "n/d/s"; exit 1;;
+	*) echo "n/d/s" >&2; exit 1;;
 	esac
 fi
+
+case "$VERSION" in
+"fit14nightly");;
+"fit14devel");;
+"fit14stable");;
+*) echo "version must be fit14stable, fit14devel or fit14nightly" >&2; exit 1;;
+esac
 
 if [ -f /vagrant/setup/switch-version-internal.sh ]; then
 	sudo bash /vagrant/setup/switch-version-internal.sh $VERSION
 else
-	vagrant ssh -c "sudo bash /vagrant/setup/switch-version-internal.sh $VERSION"
+	vagrant ssh -c "sudo bash /vagrant/setup/switch-version-internal.sh $VERSION" -- -q
 fi
 
