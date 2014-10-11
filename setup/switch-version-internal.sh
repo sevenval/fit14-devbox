4905f9b787dae5a6004e2940b74ae4496d7a99f8
Felix Hassert <felix.hassert@sevenval.com>
2014-10-11 12:44:01 +0200

    switch-version findet `.rpmsave` files auch in tieferen verzeichnissen. bisher wurde nur in conf/ geglobbt.

--- setup/switch-version-internal.sh
+++ setup/switch-version-internal.sh
@@ -12,11 +12,12 @@ yum-config-manager --quiet --disable fit14* >/dev/null
 yum-config-manager --quiet --enable $1 >/dev/null
 yum install fit14fitserver 
 cd /opt/sevenval/fit14/conf
-for i in *; do
-	if [ -f "$i.rpmsave" ]; then
-		mv "$i.rpmsave" $i
-	fi
+
+for i in $(find /opt/sevenval/fit14/conf -type f -name \*.rpmsave); do
+	mv $i ${i%.rpmsave}
+	echo "* restoring changed config file /opt/sevenval/fit14/conf/${i}"
 done
+
 /opt/sevenval/fit14/bin/fitadmin config generate
 /opt/sevenval/fit14/sbin/apachectl start
 /opt/sevenval/fit14/sbin/phpfpmctl start
