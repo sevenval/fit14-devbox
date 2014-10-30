c045c16073df24fde0b8a40a3ac7307356792e45
Felix Hassert <felix.hassert@sevenval.com>
2014-10-30 21:37:56 +0100

    box RC mit 14.0.2 RC2
    Achtung: neues image - destroy und up noetig

--- setup/check_internal.sh
+++ setup/check_internal.sh
@@ -200,7 +200,7 @@ fi
 
 # ====================================================================================================
 
-EXP_VERSION="14-0-1"
+EXP_VERSION="14-0-2-c2"
 FIT_VERSION=`sudo /opt/sevenval/fit14/bin/fitadmin -v | head -n1`
 
 sMessage="FIT Version '$EXP_VERSION' (is: $FIT_VERSION)"
