edb5cca49a4b33fb5c172abdf7b7c0b36ed4dce8
Felix Hassert <felix.hassert@sevenval.com>
2015-04-09 17:59:19 +0200

    version bump in setup-check, see #26976 14.0.6 devbox

--- setup/check_internal.sh
+++ setup/check_internal.sh
@@ -182,7 +182,7 @@ fi
 
 # ====================================================================================================
 
-EXP_VERSION="14-0-6-c1"
+EXP_VERSION="14-0-6"
 FIT_VERSION=`sudo /opt/sevenval/fit14/bin/fitadmin -v | head -n1`
 
 sMessage="FIT Version '$EXP_VERSION' (is: $FIT_VERSION)"
