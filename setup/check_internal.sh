fe369a0dc94ae2362aa37e51fe251cad6320b824
Felix Hassert <felix.hassert@sevenval.com>
2015-08-04 20:14:53 +0200

    Box Update to 14.1.1, see #28447

--- setup/check_internal.sh
+++ setup/check_internal.sh
@@ -177,7 +177,7 @@ fi
 
 # ====================================================================================================
 
-EXP_VERSION="14-1-0"
+EXP_VERSION="14-1-1"
 FIT_VERSION=`sudo /opt/sevenval/fit14/bin/fitadmin -v | head -n1`
 
 sMessage="FIT Version '$EXP_VERSION' (is: $FIT_VERSION)"
