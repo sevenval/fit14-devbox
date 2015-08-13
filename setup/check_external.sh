1f940fa57ddaff9f0df91eb56453d729ef13bb4b
Felix Hassert <felix.hassert@sevenval.com>
2015-08-13 18:36:40 +0200

    tests fuer neue domains.xml, see #28593-wildcard

--- setup/check_external.sh
+++ setup/check_external.sh
@@ -25,6 +25,51 @@ fi
 
 # ====================================================================================================
 
+iCheck=`curl -m 3 -k -s 'http://local14.sevenval-fit.com/' | grep -c 'Welcome'`
+sMessage="Domain check (local14.sevenval-fit.com:80)"
+if [ "$iCheck" -gt 0 ]; then
+	_printLine "$sMessage" 1
+else
+	_printLine "$sMessage" 0
+fi
+
+# ====================================================================================================
+iCheck=`curl -m 3 -k -s 'https://local14.sevenval-fit.com/' | grep -c 'Welcome'`
+sMessage="Domain check (local14.sevenval-fit.com:443)"
+if [ "$iCheck" -gt 0 ]; then
+	_printLine "$sMessage" 1
+else
+	_printLine "$sMessage" 0
+fi
+
+# ====================================================================================================
+iCheck=`curl -m 3 -k -s 'https://_default.local14.sevenval-fit.com/' | grep -c 'Welcome'`
+sMessage="Domain check (*.local14.sevenval-fit.com:443)"
+if [ "$iCheck" -gt 0 ]; then
+	_printLine "$sMessage" 1
+else
+	_printLine "$sMessage" 0
+fi
+
+# ====================================================================================================
+iCheck=`curl -m 3 -k -s 'https://_default._default.local14.sevenval-fit.com/' | grep -c 'Welcome'`
+sMessage="Domain check (*.*.local14.sevenval-fit.com:443)"
+if [ "$iCheck" -gt 0 ]; then
+	_printLine "$sMessage" 1
+else
+	_printLine "$sMessage" 0
+fi
+
+# ====================================================================================================
+sMessage="Domain check (*.*.local14.sevenval-fit.com:443)"
+if curl -m 3 -k -s -f 'https://not.there.local14.sevenval-fit.com/' > /dev/null; then
+	_printLine "$sMessage" 0
+else
+	_printLine "$sMessage" 1
+fi
+
+# ====================================================================================================
+
 iCheck=`curl -m 3 -k -s 'http://local14.sevenval-fit.com:8080/test.fit' | grep -c 'Overall: alive'`
 
 sMessage="Config check (calling /test.fit per hostname and HTTP)"
