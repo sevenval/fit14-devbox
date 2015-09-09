43547b0a3e9853f1c060e15189ed5896d4a4e4d3
Sebastian Thielen <sebastian.thielen@sevenval.com>
2015-09-09 11:44:26 +0200

    Box update to FIT 14.1.2

--- setup/start-services.sh
+++ setup/start-services.sh
@@ -1,11 +1,20 @@
 if service fit14apache status >/dev/null; then service fit14apache stop; fi; service fit14apache start > /dev/null
 if service fit14phpfpm status >/dev/null; then service fit14phpfpm stop; fi; service fit14phpfpm start > /dev/null
 
-echo
 sudo /opt/sevenval/fit14/bin/fitadmin -v
-echo
-echo access your local FIT at
-echo 
-echo http://local14.sevenval-fit.com:8080/
-echo https://local14.sevenval-fit.com:8443/
-echo 
+
+cat <<EOF
+
+access your local FIT at
+
+http://local14.sevenval-fit.com/
+https://local14.sevenval-fit.com/
+
+or
+
+http://yourproject.local14.sevenval-fit.com/
+https://yourproject.local14.sevenval-fit.com/
+
+more info at https://developer.sevenval.com/
+
+EOF
