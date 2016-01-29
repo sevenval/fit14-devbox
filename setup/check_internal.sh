0b9539d8b40038724c6ef1d0efd1efb3a6170100
Sebastian Thielen <sebastian.thielen@sevenval.com>
2016-01-29 16:12:49 +0100

    #30642 neue sdk version

--- setup/check_internal.sh
+++ setup/check_internal.sh
@@ -177,7 +177,7 @@ fi
 
 # ====================================================================================================
 
-EXP_VERSION=14.2.0-c1
+EXP_VERSION=14.2.0-c2
 FIT_VERSION=`sudo /opt/sevenval/fit14/bin/fitadmin -v | head -n1`
 
 sMessage="FIT Version '$EXP_VERSION' (is: $FIT_VERSION)"
